c3c1749f38da10cf574e26de43b5379480e6f2c8
Rayane <rayane.belbachir@student.vinci.be>
2023-11-11 17:18:11 +0100

    add : case name === null

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,14 +1,23 @@
-import { Main } from '../src/app.js'
+import { Main } from "../src/app.js";
 
-describe('greet', function () {
-  it('demo function greet person', function () {
+describe("greet", function () {
+  it("demo function greet person", function () {
     // Arrange
-    const main = new Main()
-
+    const main = new Main();
     // Act
-    const result = main.greet('Bob')
+    const result = main.greet("Bob");
 
     // Assert
-    expect(result).toBe('Hello, Bob')
-  })
-})
+    expect(result).toBe("Hello, Bob.");
+  });
+
+  it("function greet return hello, my friend when the name is null", function () {
+    // Arrange
+    const main = new Main();
+    // Act
+    const result = main.greet(null);
+
+    // Assert
+    expect(result).toBe("Hello, my friend.");
+  });
+});
