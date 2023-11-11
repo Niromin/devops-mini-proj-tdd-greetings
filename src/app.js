5d37897d46a30fc52cfd160636d962f0bed9d5dd
QuentinPlatiau <quentin.platiau@student.vinci.be>
2023-11-11 18:17:18 +0100

    refactor : removing case where name is null in the function greet

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
 export class Main {
   greet (name, nameBis) {
     if (name === null || name === undefined || name === '') {
-      if (nameBis === null || nameBis === undefined || nameBis === '') {
-        return 'Hello, my friend.'
-      }
-      if (nameBis === nameBis.toUpperCase()) {
-        return 'HELLO, ' + nameBis + '.'
-      }
-      return 'Hello, ' + nameBis + '.'
+      return 'Hello, my friend.'
     } else {
       if (nameBis === null || nameBis === undefined || nameBis === '') {
         if (name === name.toUpperCase()) {
