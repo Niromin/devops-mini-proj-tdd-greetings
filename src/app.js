8faf170da6ca7e1f7fabf62072e713586bab40d6
QuentinPlatiau <quentin.platiau@student.vinci.be>
2023-11-13 15:08:49 +0100

    7.Managing_multiple_languages

--- src/app.js
+++ src/app.js
@@ -3,27 +3,66 @@ export class Main {
     if (!names) {
       return 'Hello, my friend.'
     }
-    names = names.filter(
-      (name) => name !== null && name !== undefined && name !== ''
-    )
+
+    names = names.filter((name) => name !== null && name !== undefined && name !== '')
 
     if (names.length === 0) {
       return 'Hello, my friend.'
+    }
+
+    let language = 'en'
+    if (names[names.length - 1] === 'fr' || names[names.length - 1] === 'en' || names[names.length - 1] === 'nl') {
+      language = names.pop()
+    }
+    if (names.length === 0) {
+      if (language === 'fr') return 'Bonjour, mon ami.'
+      if (language === 'en') return 'Hello, my friend.'
+      if (language === 'nl') return 'Hallo, mijn vriend.'
     }
 
     const upper = names.filter((name) => name.toUpperCase() === name)
     const normal = names.filter((name) => name.toUpperCase() !== name)
 
     let message
-    let messageNormal = 'Hello, '
-    let messageUpper = 'HELLO, '
+    let messageNormal
+    let messageUpper
+
+    const messageNormalLangue = {
+      fr: 'Bonjour, ',
+      en: 'Hello, ',
+      nl: 'Hallo, '
+    }
+
+    const messageUpperLanguage = {
+      fr: 'BONJOUR, ',
+      en: 'HELLO, ',
+      nl: 'HALLO, '
+    }
+
+    const andLanguague = {
+      fr: 'et',
+      en: 'and',
+      nl: 'end, '
+    }
+
+    const andUpperLanguage = {
+      fr: 'ET',
+      en: 'AND',
+      nl: 'END'
+    }
+
+    messageNormal = messageNormalLangue[language]
+    messageUpper = messageUpperLanguage[language]
+    const and = andLanguague[language]
+    const andUpper = andUpperLanguage[language]
+
     for (let index = 0; index < normal.length; index++) {
       if (index === 0) {
         messageNormal += normal[index]
       } else if (normal.length > 2 && index !== normal.length - 1) {
         messageNormal += ', ' + normal[index]
       } else {
-        messageNormal += ' and ' + normal[index]
+        messageNormal += ` ${and} ` + normal[index]
       }
     }
     messageNormal += '.'
@@ -34,7 +73,7 @@ export class Main {
       } else if (upper.length > 2 && index !== upper.length - 1) {
         messageUpper += ', ' + upper[index]
       } else {
-        messageUpper += ' AND ' + upper[index]
+        messageUpper += ` ${andUpper} ` + upper[index]
       }
     }
     messageUpper += '!'
@@ -46,7 +85,7 @@ export class Main {
       message = messageUpper
     }
     if (normal.length > 0 && upper.length > 0) {
-      message = messageNormal + ' AND ' + messageUpper
+      message = messageNormal + ` ${andUpper} ` + messageUpper
     }
 
     return message
