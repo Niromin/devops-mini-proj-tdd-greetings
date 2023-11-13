f0ecdedecd7a9443d88f7a8542107cbfcbf0c83e
QuentinPlatiau <quentin.platiau@student.vinci.be>
2023-11-13 12:18:13 +0100

    refactor : condition correction

--- src/app.js
+++ src/app.js
@@ -1,54 +1,54 @@
 export class Main {
   greet (names) {
-    if (!names || names.length === 0 || names[0] === '') {
+    if (!names) {
       return 'Hello, my friend.'
     }
     names = names.filter(
       (name) => name !== null && name !== undefined && name !== ''
     )
 
+    if (names.length === 0) {
+      return 'Hello, my friend.'
+    }
+
     const upper = names.filter((name) => name.toUpperCase() === name)
     const normal = names.filter((name) => name.toUpperCase() !== name)
-    
-    
+
     let message
     let messageNormal = 'Hello, '
     let messageUpper = 'HELLO, '
-    if (normal.length > 0){
-        for (let index = 0; index < normal.length; index++) {
-            if (index === 0) {
-              messageNormal += normal[index]
-            } else if (normal.length > 2 && index !== (normal.length - 1)) {
-              messageNormal += ', ' + normal[index]
-            } else {
-              messageNormal += ' and ' + normal[index]
-            }
-          }
-          messageNormal += '.'
+    for (let index = 0; index < normal.length; index++) {
+      if (index === 0) {
+        messageNormal += normal[index]
+      } else if (normal.length > 2 && index !== normal.length - 1) {
+        messageNormal += ', ' + normal[index]
+      } else {
+        messageNormal += ' and ' + normal[index]
+      }
     }
-    
-    if (upper.length > 0){
-        for (let index = 0; index < upper.length; index++) {
-            if (index === 0) {
-                messageUpper += upper[index]
-            } else if (upper.length > 2 && index !== (upper.length - 1)) {
-                messageUpper += ', ' + upper[index]
-            } else {
-                messageUpper += ' AND ' + upper[index]
-            }
-        }
-        messageUpper += '!'
+    messageNormal += '.'
+
+    for (let index = 0; index < upper.length; index++) {
+      if (index === 0) {
+        messageUpper += upper[index]
+      } else if (upper.length > 2 && index !== upper.length - 1) {
+        messageUpper += ', ' + upper[index]
+      } else {
+        messageUpper += ' AND ' + upper[index]
+      }
     }
-    if (normal.length > 0){
-        message = messageNormal
+    messageUpper += '!'
+
+    if (normal.length > 0 && upper.length === 0) {
+      message = messageNormal
     }
-    if (upper.length > 0){
-        message = messageUpper
+    if (upper.length > 0 && normal.length === 0) {
+      message = messageUpper
     }
-    if (normal.length > 0 && upper.length > 0){
-        message = messageNormal + ' AND ' + messageUpper
+    if (normal.length > 0 && upper.length > 0) {
+      message = messageNormal + ' AND ' + messageUpper
     }
-    
+
     return message
   }
 }
